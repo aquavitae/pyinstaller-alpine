ed6523ca59a8dc9b239902a3329b88e59ba14ace
David Townshend <aquavitae69@gmail.com>
2019-05-21 13:24:11 +0200

    fix image tag

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export PYTHON_VERSION=${1}
 export ALPINE_VERSION=${2:-3.8}
 export PYINSTALLER_TAG=${3:-v3.4}
 
-REPO="aquavitae/pyinstaller-alpine:$PYTHON_VERSION"
+REPO="aquavitae/pyinstaller-alpine:$PYTHON_VERSION-$ALPINE_VERSION"
 
 echo "python: $PYTHON_VERSION"
 echo "alpine: $ALPINE_VERSION"
